c8aeb6badfa53a46759c59bf9475b62de79a6dba
mnebel <mxnbl@me.com>
2013-07-18 10:43:51 -0500

    other h files

---
+++ TheCabin/TheCabin/character.h
@@ -0,0 +1,32 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include <iostream>
+#include <string>
+#include "GL\glew.h"
+#include "GL\freeglut.h"
+#include "IL\il.h"
+#include "AL\al.h"
+#include "AL\alut.h"
+
+using namespace std;
+
+class Character
+{
+public:
+string hair[3];
+string eyes[3];
+string face[3];
+string mouth[3];
+string name;
+
+void draw();
+void loadTexture();
+void update(int value);
+void saveToFile();
+void loadFromFile();
+
+
+};
+
+#endif
---
+++ TheCabin/TheCabin/objects.h
@@ -0,0 +1,17 @@
+#ifndef OBJECTS_H
+#define OBJECTS_H
+
+#include <iostream>
+#include <string>
+#include "GL\glew.h"
+#include "GL\freeglut.h"
+#include "IL\il.h"
+#include "AL\al.h"
+#include "AL\alut.h"
+
+class Objects
+{
+
+};
+
+#endif

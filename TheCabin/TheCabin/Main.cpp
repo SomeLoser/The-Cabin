66cc5e58c6073dda16c6aa2a38b92c1a33ded97e
mnebel <mxnbl@me.com>
2013-07-18 08:30:04 -0500

    file stuff

---
+++ TheCabin/TheCabin/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "GL\glew.h"
+#include "GL\freeglut.h"
+#include "IL\il.h"
+#include "AL\al.h"
+#include "AL\alut.h"
+
+
+int main(int argc, int &argv)
+{
+
+}
